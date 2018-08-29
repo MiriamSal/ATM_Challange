2a7d18f09b24a4b7ec7f451ef526307531e6c5d0
danmalmx <danmalmx@gmail.com>
2018-08-29 20:31:23 +0200

    methods to check activation status of account

--- lib/account.rb
+++ lib/account.rb
@@ -2,14 +2,14 @@ class Account
 
     STANDARD_VALIDITY_YRS = 5
 
-    attr_accessor :owner, :exp_date
+    attr_accessor :owner, :exp_date, :account_status
 
     def initialize(attrs = {})
         @pin_code = rand(1000..9999)
         set_owner(attrs[:owner])
         @exp_date = set_expire_date
+        @account_status = :active
     end
-
 
     private
 
@@ -25,5 +25,12 @@ class Account
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
+    # def deactivate
+    #     @account_status = :deactivated
+    # end
+
+    def self.deactivate(account)
+        account.account_status = :deactivated
+    end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -19,14 +19,25 @@ describe Account do
         number = rand(1000..9999)
         number_length = Math.log10(number).to_i + 1
         expect(number_length).to eq 4
-      end
+    end
 
     it 'is ecpected to have an expiry date on initialize' do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
         expect(subject.exp_date).to eq expected_date
     end
 
+    it "is expected to have :active status on initialize" do
+        expect(subject.account_status).to eq :active
+    end
 
+    # it "deactivates account using Instance method" do
+    #     subject.deactivate
+    #     expect(subject.account_status).to eq :deactivated
+    # end
 
+    it "deactivates account using Class method" do
+        Account.deactivate(subject)
+        expect(subject.account_status).to eq :deactivated
+    end
 end
 
